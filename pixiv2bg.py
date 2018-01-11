3a65b3ab5d36cdef92f816ec07571bae52af6238
Nasy <sy_n@me.com>
2018-01-12 03:29:02 +0800

    a stable and newer crawer

--- pixiv2bg.py
+++ pixiv2bg.py
@@ -28,8 +28,8 @@ Excited without bugs::
     |  ___|______|______|______|______|______|____
 
 * author: Nasy
-* date: Oct 12, 2017
-* email: sy_n@me.com
+* date: Jan 11, 2018
+* email: echo bmFzeXh4QGdtYWlsLmNvbQo= | base64 -D
 * file: pixiv2bg.py
 * license: MIT
 
@@ -37,7 +37,7 @@ Copyright © 2017 by Nasy. All Rights Reserved.
 """
 import asyncio
 import os
-from typing import Any, Dict, Generator
+from typing import Any, Awaitable, Dict, List, Optional
 
 import aiohttp
 import uvloop
@@ -46,117 +46,121 @@ from tqdm import tqdm
 import ujson as json
 from config import settings
 
+assert Awaitable
+assert Optional
+
 URL = "https://api.pixiv.moe/v1/ranking?page="
 SETTINGS = settings()
+WKS = List[Dict[str, Any]]
 
 
-async def illusts_filter(illusts: Dict[str, Any]) -> bool:
-    """Filter the illusts."""
-    work = illusts["work"]
-    width, height, tags = work["width"], work["height"], work["tags"]
-    for tag in tags:
-        if tag in SETTINGS["filter"]["tags"]:
+class Pixiv:
+    """Crawer of pixiv, craw pictures of pixiv for backgrouds."""
+
+    def _load(self) -> Dict[str, Any]:
+        """Load stored pictures."""
+        with open(SETTINGS["file"]) as s:
+            return json.load(s)
+
+    def __init__(self) -> None:
+        """Initialize Pixiv."""
+        self.loop = uvloop.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.sema = asyncio.Semaphore(5)
+        self.works = []  # type: List[Awaitable[None]]
+        self.fetchs = [
+            asyncio.ensure_future(self._fetch_page(p), loop = self.loop)
+            for p in range(1, 11)
+        ]
+        self.stores = self._load()
+        self.tqdm = None  # type: Optional[tqdm]
+
+    async def _illusts_filter(self, illusts: Dict[str, Any]) -> bool:
+        """Filter the illusts."""
+        work = illusts["work"]
+        width, height, tags = work["width"], work["height"], work["tags"]
+        for tag in tags:
+            if tag in SETTINGS["filter"]["tags"]:
+                return False
+        if (width < SETTINGS["filter"]["min_width"] or
+                height < SETTINGS["filter"]["min_height"] or
+                width / height < SETTINGS["filter"]["min_w2h"] or
+                width / height > SETTINGS["filter"]["max_w2h"]):
             return False
-    if (width < SETTINGS["filter"]["min_width"] or
-            height < SETTINGS["filter"]["min_height"] or
-            width / height < SETTINGS["filter"]["min_w2h"] or
-            width / height > SETTINGS["filter"]["max_w2h"]):
-        return False
-    return True
+        return True
 
-
-async def fetch_page(page: int = 1) -> Dict[str, Any]:
-    """Fetch pixiv.moe page."""
-    async with aiohttp.ClientSession() as session:
-        async with session.get(URL + str(page)) as res:
-            return await res.json()
-
-
-async def fetch_illust(url: str, title: str):
-    """Fetch illusts."""
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url,
+    async def _fetch_page(self, page: int = 1) -> None:
+        """Fetch pixiv.moe page."""
+        async with aiohttp.ClientSession() as session:
+            async with session.get(URL + str(page),
                                    proxy = "http://127.0.0.1:6152") as res:
-                try:
-                    os.mkdir("pictures")
-                except FileExistsError:
-                    pass
-                chunks = b""
-                while True:
-                    chunk = await res.content.read(5)
-                    if not chunk:
-                        break
-                    chunks = chunks + chunk
-        except asyncio.TimeoutError:
-            return
-        with open((f"pictures/{title.replace('/','-')}."
-                   f"{url[-4:].split('.')[-1]}"), "wb") as f:
-            f.write(chunks)
+                content = await res.json()
+                if content.get("status") == "success":
+                    work = content["response"]["works"]
+                    await self._get_urls(work)
+                    self.tqdm.update()
 
+    async def _get_urls(self, works) -> None:
+        """Get urls from works."""
+        for illust in works:
+            if (((not self.stores) or
+                 str(illust["work"]["id"]) not in self.stores) and
+                    await self._illusts_filter(illust)):
+                self.works.append(
+                    asyncio.ensure_future(
+                        self.fetch_illust(
+                            illust["work"]["image_urls"]["large"],
+                            illust["work"]["title"],
+                            str(illust["work"]["id"]),
+                        )
+                    )
+                )
+                self.stores.update({str(illust["work"]["id"]): illust})
 
-async def add_illust(illust: Dict[str, Any], stores: Dict[str, Any],
-                     t: Any) -> Dict[str, Any]:
-    """Add illusts."""
-    if ((not stores or str(illust["work"]["id"]) not in stores) and
-            await illusts_filter(illust)):
-        try:
-            await fetch_illust(
-                illust["work"]["image_urls"]["large"], illust["work"]["title"]
-            )
-            t.update()
-            return illust["work"]
-        except aiohttp.client_exceptions.ServerDisconnectedError:
-            pass
-    t.update()
-    return {"id": None}
+    async def fetch_illust(self, url: str, title: str, workid: str) -> None:
+        """Fetch illusts."""
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with self.sema:
+                    async with session.get(
+                            url,
+                            proxy = "http://127.0.0.1:6152",
+                    ) as res:
+                        try:
+                            os.mkdir("pictures")
+                        except FileExistsError:
+                            pass
+                        chunks = b""
+                        while True:
+                            chunk = await res.content.read(128)
+                            if not chunk:
+                                break
+                            chunks = chunks + chunk
+            except asyncio.TimeoutError:
+                self.stores.pop(workid)
+            with open((f"pictures/{title.replace('/','-')}."
+                       f"{url[-4:].split('.')[-1]}"), "wb") as f:
+                f.write(chunks)
+            self.tqdm.update()
+
+    def run(self) -> None:
+        """Run this crawer."""
+        self.tqdm = tqdm(total = 10)
+        self.loop.run_until_complete(asyncio.wait(self.fetchs))
+        self.tqdm.close()
+
+        self.tqdm = tqdm(total = len(self.works))
+        self.loop.run_until_complete(asyncio.wait(self.works))
+        self.tqdm.close()
+        with open(SETTINGS["file"] + ".temp", "w") as f:
+            json.dump(self.stores, f)
+        os.replace(SETTINGS["file"] + ".temp", SETTINGS["file"])
 
 
 def main() -> None:
-    """Run this task."""
-    stores = {}  # type: Dict[str, Any]
-    if SETTINGS.get("store", True):
-        try:
-            with open(SETTINGS["file"]) as s:
-                stores = json.load(s)  # pylint: disable=E1101
-        except FileNotFoundError:
-            pass
-        except ValueError:
-            pass
-    else:
-        pass
-
-    loop = uvloop.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    fetchs = [
-        asyncio.ensure_future(fetch_page(p + 1))
-        for p in range(int(SETTINGS["pages"]))
-    ]
-
-    loop.run_until_complete(asyncio.wait(fetchs))
-    works = [fetch.result()["response"]["works"] for fetch in fetchs]
-
-    def _works() -> Generator[Dict[str, Any], None, None]:
-        """Generate work from works."""
-        for page in works:
-            for work in page:
-                yield work
-
-    with tqdm(total = len([_ for _ in _works()])) as t:
-        tasks = [
-            asyncio.ensure_future(add_illust(work, stores, t))
-            for work in _works()
-        ]
-        loop.run_until_complete(asyncio.wait(tasks))
-    if SETTINGS["store"]:
-        with open(SETTINGS["file"] + ".temp", "w") as f:
-            for task in tasks:
-                result = task.result()
-                if result["id"]:
-                    stores[result["id"]] = result
-            json.dump(stores, f)  # pylint: disable=E1101
-        os.replace(SETTINGS["file"] + ".temp", SETTINGS["file"])
+    """Yooo, here is the main function."""
+    pixiv = Pixiv()
+    pixiv.run()
 
 
 if __name__ == "__main__":
